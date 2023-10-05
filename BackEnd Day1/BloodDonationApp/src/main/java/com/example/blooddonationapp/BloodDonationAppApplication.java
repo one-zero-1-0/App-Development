3e321e49b9800871672166929363c9a6c991ee98
Arul Edwin A <110535601+one-zero-1-0@users.noreply.github.com>
2023-10-05 09:54:51 +0530

    Add files via upload

---
+++ BackEnd Day1/BloodDonationApp/src/main/java/com/example/blooddonationapp/BloodDonationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.blooddonationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationAppApplication.class, args);
+	}
+
+}
